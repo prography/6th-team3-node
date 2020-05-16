178922f00412fc9ab05d657a92c03ccd064b84e7
emiling <scopradical@gmail.com>
2020-05-17 01:22:42 +0900

    Add mysql and node docker environment in dev

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import config from './config';
 import Logger from './loaders/logger';
 import app from './app';
 
-const HOST = config.host || 'localhost';
+const HOST = config.host || '0.0.0.0';
 const PORT = Number(config.port || 1234);
 
 const startApplication = async () => {
-  app.listen(PORT, HOST, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     Logger.info(`server is running on ${HOST}:${PORT}`);
+    // console.log()
   });
 };
 
