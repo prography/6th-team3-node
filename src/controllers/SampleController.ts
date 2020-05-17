7e46d49c09ff8c3f29034d9f326b387cfaef8177
emiling <scopradical@gmail.com>
2020-05-18 05:49:21 +0900

    Add prisma setting and ORM mapping

--- src/controllers/SampleController.ts
+++ src/controllers/SampleController.ts
@@ -1,8 +1,14 @@
 import { BaseController } from './BaseController';
+import { PrismaClient } from '@prisma/client';
 import { JsonController, Get } from 'routing-controllers';
 
 @JsonController('/')
 export class TodoController extends BaseController {
+  private databaseClient: PrismaClient;
+  constructor() {
+    super();
+    this.databaseClient = new PrismaClient();
+  }
   @Get()
   public index() {
     return 'Hello! This is sample😎';
