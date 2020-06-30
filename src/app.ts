0927728657e766a7b96ad963b2dbc1b8d99913d1
emiling <scopradical@gmail.com>
2020-07-01 04:01:46 +0900

    Delete URL prefix for subdomain

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import config from './config';
 import { useExpressServer } from 'routing-controllers';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 useExpressServer(app, {
   cors: true,
-  routePrefix: config.api.prefix,
   controllers: [`${__dirname}/controllers/**`],
 });
 
